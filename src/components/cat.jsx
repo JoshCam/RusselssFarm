3ad83c182a390b568b4ad6f14c30c3b186698b88
JoshCam <joshcampden15@gmail.com>
2021-06-19 14:07:19 +0100

    added pics

--- src/components/cat.jsx
+++ src/components/cat.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import picture from "../img/cat.svg";
 
 class Cat extends Component {
   render() {
-    return <div>Cat</div>;
+    return (
+      <div className="container">
+        <div>
+          <span>C</span>
+          <span>A</span>
+          <span>T</span>
+        </div>
+        <div>
+          <h1>Listen</h1>
+        </div>
+        <div>
+          <img src={picture}></img>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/cow.jsx
+++ src/components/cow.jsx
@@ -3,7 +3,21 @@ import { connect } from "react-redux";
 
 class Cow extends Component {
   render() {
-    return <div>Cow</div>;
+    return (
+      <div className="container">
+        <div>
+          <span>C</span>
+          <span>O</span>
+          <span>W</span>
+        </div>
+        <div>
+          <h1>Listen</h1>
+        </div>
+        <div>
+          <img src={picture}></img>
+        </div>
+      </div>
+    );
   }
 }
 
